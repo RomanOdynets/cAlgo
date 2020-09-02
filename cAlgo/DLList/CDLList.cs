7648ae7fe5c5d3e86c7f80f8d60a60b4b1ae9daf
Roman Odynets <r.chernenko28@gmail.com>
2020-09-02 20:51:30 +0300

    Circle added

---
+++ cAlgo/DLList/CDLList.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace cAlgo.DLList
+{
+    /// <summary>
+    /// Двухсторонний связной список, содержит ссылку на предыдущий обьект, информацию и ссылку на следующий элемент списка
+    /// </summary>
+    /// <typeparam name="T">Тип данных хранимый в списке</typeparam>
+    public class CDLList<T> : IEnumerable<T>
+    {
+        private DoublyNode<T> head;
+        private int count;
+
+        /// <summary>
+        /// Добавление в конец списка значения по шаблону
+        /// </summary>
+        /// <param name="data">Записываемая информация по шаблону</param>
+        public void Add(T data)
+        {
+            DoublyNode<T> node = new DoublyNode<T>(data);
+
+            if(head == null)
+            {
+                head = node;
+                head.Next = node;
+                head.Previous = node;
+            }
+            else
+            {
+                node.Previous = head.Previous;
+                node.Next = head;
+                head.Previous.Next = node;
+                head.Previous = node;
+            }
+            count++;
+        }
+
+        /// <summary>
+        /// Удаление информации со списка
+        /// </summary>
+        /// <param name="data">Данные, которые требуется удалить</param>
+        /// <returns>Результат удаления</returns>
+        public bool Remove(T data)
+        {
+            DoublyNode<T> current = head;
+
+            DoublyNode<T> removedItem = null;
+
+            if (count == 0) return false;
+
+            do
+            {
+                if(current.Data.Equals(data))
+                {
+                    removedItem = current;
+                    break;
+                }
+                current = current.Next;
+            } while (current != head);
+
+            if(removedItem != null)
+            {
+                if (count == 1) head = null;
+                else
+                {
+                    if(removedItem == head)
+                    {
+                        head = head.Next;
+                    }
+                    removedItem.Previous.Next = removedItem.Next;
+                    removedItem.Next.Previous = removedItem.Previous;
+                }
+                count--;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Количество элементов в списке
+        /// </summary>
+        public int Count { get { return count; } }
+
+        /// <summary>
+        /// Возвращает пустой ли список
+        /// </summary>
+        public bool IsEmpty { get { return count == 0; } }
+
+
+        /// <summary>
+        /// Очищает список от всех элементов
+        /// </summary>
+        public void Clear()
+        {
+            head = null;
+            count = 0;
+        }
+
+        /// <summary>
+        /// Проверка на существование информации в списке
+        /// </summary>
+        /// <param name="data">Искомая информация по шаблону</param>
+        /// <returns>Результат поиска</returns>
+        public bool Contains(T data)
+        {
+            DoublyNode<T> current = head;
+            if (current == null) return false;
+            do
+            {
+                if (current.Data.Equals(data)) return true;
+                current = current.Next;
+            } while (current != head);
+            return false;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            DoublyNode<T> current = head;
+            do
+            {
+                if(current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            }while(current != head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        /// <summary>
+        /// Вывод элементов списка в обратном порядке
+        /// </summary>
+        /// <returns>Интерфейс списка</returns>
+        public IEnumerable<T> BackEnumerator()
+        {
+            DoublyNode<T> current = head;
+
+            do
+            {
+                if (current != null)
+                {
+                    yield return current.Data;
+                    current = current.Previous;
+                }
+            } while (current != head);
+        }
+    }
+}
---
+++ cAlgo/LList/CLList.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace cAlgo.LList
+{
+    /// <summary>
+    /// Связной список
+    /// </summary>
+    /// <typeparam name="T">Тип данных хранимый в списке</typeparam>
+    public class CLList<T> : IEnumerable<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        private int count;
+
+        /// <summary>
+        /// Добавление информации в конец списка
+        /// </summary>
+        /// <param name="data">Записываемая информация по шаблону</param>
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+                tail = node;
+                tail.Next = head;
+            }
+            else
+            {
+                node.Next = head;
+                tail.Next = node;
+                tail = node;
+            }
+
+            count++;
+        }
+
+        /// <summary>
+        /// Удаление информации из списка
+        /// </summary>
+        /// <param name="data">Удаляемая информация по шаблону</param>
+        /// <returns>Результат удаления</returns>
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+
+            if (IsEmpty) return false;
+
+            do
+            {
+                if(current.Data.Equals(data))
+                {
+                    if(previous != null)
+                    {
+                        previous.Next = current.Next;
+                        if (current == tail) tail = previous;
+                    }
+                    else
+                    {
+                        if(count == 1)
+                        {
+                            head = tail = null;
+                        }
+                        else
+                        {
+                            head = current.Next;
+                            tail.Next = current.Next;
+                        }
+                    }
+                    count--;
+                    return true;
+                }
+                previous = current;
+                current = current.Next;
+            } while (current != head);
+
+            return false;
+        }
+
+        /// <summary>
+        /// Количество элементов в списке
+        /// </summary>
+        public int Count { get { return count; } }
+
+        /// <summary>
+        /// Возвращает пустой ли список
+        /// </summary>
+        public bool IsEmpty { get { return count == 0; } }
+
+        /// <summary>
+        /// Очищает список от всех элементов
+        /// </summary>
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        /// <summary>
+        /// Проверяет существует ли искомая информация в списке
+        /// </summary>
+        /// <param name="data">Искомая информация по шаблону</param>
+        /// <returns>Результат поиска</returns>
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            do
+            {
+                if (current.Data.Equals(data)) return true;
+                current = current.Next;
+            } while (current != head);
+            return false;
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            do
+            {
+                if(current != null)
+                {
+                    yield return current.Data;
+                    current = current.Next;
+                }
+            } while (current != head);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+    }
+}
